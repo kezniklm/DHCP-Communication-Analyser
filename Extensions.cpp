2e6e6f953cf39588018d3ccb6a017ffedb540b10
Matej Keznikl <matej.keznikl@gmail.com>
2023-10-05 11:48:08 +0200

    Removed Extensions class files

--- Extensions.cpp
+++
@@ -1,8 +0,0 @@
-/**
- * @file Extensions.cpp
- * @author Matej Keznikl (xkezni01@stud.fit.vutbr.cz)
- * @brief Implementácia triedy Extensions
- * @date 2023-11-20
- */
-
-Extensions::Extensions(){};
